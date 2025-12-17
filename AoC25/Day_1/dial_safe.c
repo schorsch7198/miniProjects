98e1132c0334e22ea5ebe4c5e7388dec82581066
Steiner Georg <georgsteiner7@gmail.com>
2025-12-17 02:33:04 +0100

    Day 1, Part 1/2

---
+++ AoC25/Day_1/dial_safe.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#define DIAL_SIZE 100
+#define START_POSITION 50
+
+/* Rotate dial and return new position */
+int rotate_dial(int pos, char dir, int dis)// pos: position | dir: direction - 'L' or 'R' | dis: distance - steps to move
+{
+    if (dir == 'R')
+    {
+        pos = (pos + dis) % DIAL_SIZE;
+    }
+    else if (dir == 'L')
+    {
+        pos = (pos - dis) % DIAL_SIZE;
+        if (pos < 0)
+        {
+            pos += DIAL_SIZE;
+        }
+    }
+    return pos;
+}
+
+/* count zeros if dial points at 0 (IMPORTANT: counts new positions at 0) */
+int count_zero(int pos, int zeros)
+{
+    if (pos == 0)
+    {
+        zeros++;
+    }
+    return zeros;
+}
+
+int main(void)
+{
+    int pos = START_POSITION;
+    int zeros = 0;
+    char dir;
+    int dis;
+    char line[32]; // Buffer for input lines
+
+    printf("\nCIRCULAR DIAL SAFE OPENER WITH COUNTER (if new position is 0)\n");
+    printf("Enter rotations (e.g. L1 or R23456), one per line.\n\n");
+
+    while (fgets(line, sizeof(line), stdin) != NULL)
+    {
+        if (sscanf(line, " %c%d", &dir, &dis) == 2)
+        {
+            pos = rotate_dial(pos, dir, dis);
+            zeros = count_zero(pos, zeros);
+        }
+        if(line[0] == '\n') // Break on empty line
+        {
+            break;
+        }
+    }
+
+    printf("Final Dial Position: %d\nNumber of Zeros (Password): %d\n\n", pos, zeros);
+    return 0;
+}
+
+// Compiling Command: gcc dial_safe.c -o safe.exe
+// Running Command: safe.exe   "or"   ./safe.exe
